c6ba567d55450646b0ea43f4c536a3a8099412bc
Vikram Rathod <rathodvikram44@gmail.com>
2021-08-22 18:11:01 +0530

    create script.sql

---
+++ script.sql
@@ -0,0 +1,8 @@
+create database CRUD;
+
+ CREATE TABLE `user` (
+    ->     `user_id` int(11) ,
+    ->     `firstName` varchar(30) ,
+    ->     `lastName` varchar(30),
+    ->     `email` varchar(30)
+    ->     );
